4575d55b39fa61fdd7f5bbfbd8cd508048c5ea6e
hyewon joo <joo7875@gmail.com>
2020-11-15 13:31:16 +0900

    Added Huinno web

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -26,3 +26,41 @@
     }
   }
     
+
+  // Image Gallery
+
+  var slideIndex = 1;
+  var slideIndex_title = 1;
+  showDivs(slideIndex);
+  showDivs_title(slideIndex_title);
+
+  function plusDivs(n) {
+    showDivs(slideIndex += n);
+    showDivs_title(slideIndex_title += n);
+  }
+
+  function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("huinno-img");
+
+    if (n > x.length) slideIndex = 1;
+    if (n < 1) slideIndex = x.length;
+
+    for (i = 0; i < x.length; i++) 
+      x[i].style.display = "none";  
+    
+    x[slideIndex-1].style.display = "block";  
+  }
+
+  function showDivs_title(n) {
+    var j;
+    var y = document.getElementsByClassName("huinno-title");
+
+    if (n > y.length) slideIndex_title = 1;
+    if (n < 1) slideIndex_title = y.length;
+
+    for (j = 0; j < y.length; j++) 
+      y[j].style.display = "none";  
+    
+    y[slideIndex_title-1].style.display = "block";  
+  }
