d886827cdcfbbcad15b40f4dc918de9990cbefdd
Hyewon Joo <hyewonjoo@Hyewons-MacBook-Pro.local>
2021-09-28 17:49:41 -0700

    Updated domain bar

--- routes/index.js
+++ routes/index.js
@@ -3,23 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hyewon Joo | Portfolio' });
+  res.render('index', { title: 'Hyewon Joo | Software Engineer' });
 });
 
 router.get('/projects/app', function(req, res, next) {
-  res.render('projects-app', { title: 'Hyewon Joo | Portfolio' });
+  res.render('projects-app', { title: 'Hyewon Joo | App' });
 });
 
 router.get('/projects/web', function(req, res, next) {
-  res.render('projects-web', { title: 'Hyewon Joo | Portfolio' });
+  res.render('projects-web', { title: 'Hyewon Joo | Web' });
 });
 
 router.get('/projects/personal', function(req, res, next) {
-  res.render('projects-personal', { title: 'Hyewon Joo | Portfolio' });
+  res.render('projects-personal', { title: 'Hyewon Joo | Projects' });
 });
 
 router.get('/reference', function(req, res, next) {
-  res.render('reference', { title: 'Hyewon Joo | Portfolio' });
+  res.render('reference', { title: 'Hyewon Joo | Reference' });
 });
 
 module.exports = router;
