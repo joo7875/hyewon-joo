7aabe4a0b5d1e0993df0fc83a978c617ea957da1
joo7875 <joo7875@gmail.com>
2024-04-05 19:53:42 +0900

    change app to mobile

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,25 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hyewon Joo | Software Engineer' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Hyewon Joo | Software Engineer" });
 });
 
-router.get('/projects/app', function(req, res, next) {
-  res.render('projects-app', { title: 'Hyewon Joo | App' });
+router.get("/projects/app", function (req, res, next) {
+  res.render("projects-app", { title: "Hyewon Joo | Mobile" });
 });
 
-router.get('/projects/web', function(req, res, next) {
-  res.render('projects-web', { title: 'Hyewon Joo | Web' });
+router.get("/projects/web", function (req, res, next) {
+  res.render("projects-web", { title: "Hyewon Joo | Web" });
 });
 
-router.get('/projects/personal', function(req, res, next) {
-  res.render('projects-personal', { title: 'Hyewon Joo | Projects' });
+router.get("/projects/personal", function (req, res, next) {
+  res.render("projects-personal", { title: "Hyewon Joo | Projects" });
 });
 
-router.get('/reference', function(req, res, next) {
-  res.render('reference', { title: 'Hyewon Joo | Reference' });
+router.get("/reference", function (req, res, next) {
+  res.render("reference", { title: "Hyewon Joo | Reference" });
 });
 
 module.exports = router;
